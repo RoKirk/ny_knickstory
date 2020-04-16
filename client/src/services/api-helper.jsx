86823d467a2336477738a17078bd2257d4566c92
Ro <hohenkirkrhol@gmail.com>
2020-04-16 17:23:23 -0400

    Troubleshooting.

--- client/src/services/api-helper.jsx
+++ client/src/services/api-helper.jsx
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000'
-
-const api = axios.create({
-    baseURL: baseUrl
+import apiUrl from './api-url'
+export const api = axios.create({
+    baseURL: apiUrl
 })
+
+// const baseUrl = 'http://localhost:3000'
+
+// const api = axios.create({
+//     baseURL: baseUrl
+// })
 
 export const loginUser = async (loginData) => {
     console.log(loginData)
